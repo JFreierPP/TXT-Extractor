9f4eea94d941ba4d3307f1a423040b4bf621d0ef
Jonathan Freier <JFreier@plugpower.com>
2023-05-30 18:12:21 -0400

    Add project files.

---
+++ TXT/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace TXT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Search = new System.Windows.Forms.TextBox();
+            this.button_Search = new System.Windows.Forms.Button();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_Search
+            // 
+            this.textBox_Search.Location = new System.Drawing.Point(12, 43);
+            this.textBox_Search.Name = "textBox_Search";
+            this.textBox_Search.Size = new System.Drawing.Size(599, 22);
+            this.textBox_Search.TabIndex = 0;
+            // 
+            // button_Search
+            // 
+            this.button_Search.Location = new System.Drawing.Point(617, 43);
+            this.button_Search.Name = "button_Search";
+            this.button_Search.Size = new System.Drawing.Size(171, 33);
+            this.button_Search.TabIndex = 1;
+            this.button_Search.Text = "Search";
+            this.button_Search.UseVisualStyleBackColor = true;
+            this.button_Search.Click += new System.EventHandler(this.button_Search_Click);
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(617, 391);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(171, 47);
+            this.button_Start.TabIndex = 2;
+            this.button_Start.Text = "Start";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_Start);
+            this.Controls.Add(this.button_Search);
+            this.Controls.Add(this.textBox_Search);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.OnLoad);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Search;
+        private System.Windows.Forms.Button button_Search;
+        private System.Windows.Forms.Button button_Start;
+    }
+}
+
---
+++ TXT/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TXT
+{
+    public partial class Form1 : Form
+    {
+        string TXTfile_Location;
+
+       
+
+        public class CardData
+        {
+            public string name { get; set; }
+            public string owner { get; set; }
+            public string ChannelName { get; set; }
+
+            public string ChannelScheme { get; set; }
+
+            public CardData()
+            {
+                this.name = string.Empty;
+                this.owner = string.Empty;
+                this.ChannelName = string.Empty;
+                this.ChannelScheme = string.Empty;
+            }
+        }
+
+        Dictionary<string, CardData> resultDict = new Dictionary<string, CardData>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            TXTfile_Location = Properties.Settings.Default.TXTfileLoc_Settings;
+            textBox_Search.Text = TXTfile_Location;
+        }
+
+        private void button_Search_Click(object sender, EventArgs e)
+        {
+            //Opening a file
+            String searchPath = "";
+            if (TXTfile_Location != "")
+            {
+                if (Directory.Exists(Path.GetDirectoryName(TXTfile_Location)))
+                    searchPath = Path.GetDirectoryName(TXTfile_Location);
+                else
+                    searchPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory); //Gets the default Deskptop location without needing the machine's name to be updated.
+            }
+            OpenFileDialog wbFile = new OpenFileDialog
+            {
+                //Properties of OpenFileDialog
+                CheckFileExists = true,
+                CheckPathExists = true,
+                InitialDirectory = searchPath
+            };
+
+            if (wbFile.ShowDialog() == DialogResult.OK)
+            {
+                TXTfile_Location = wbFile.FileName; //save the file's path to be used elsewhere in the  code
+                textBox_Search.Text = TXTfile_Location; //update the textbox to show the file path
+                Properties.Settings.Default.TXTfileLoc_Settings = TXTfile_Location; //Save location to program settings
+
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+
+            
+
+            foreach (string line in File.ReadLines(TXTfile_Location))
+            {
+                    Console.WriteLine(line);
+
+                //string[] splitSTR = line.Split(' ');
+
+            }
+
+            Console.WriteLine("\n\n");
+
+
+
+            foreach (string line in File.ReadLines(TXTfile_Location))
+            {
+
+                Console.WriteLine("\n--------------------\n");
+
+                string[] splitSTR = line.Split(null);
+
+                //TB
+                foreach(string val in splitSTR)
+                {
+                    Console.Write(val + ", ");
+                }
+                Console.WriteLine("");
+
+
+                string[] resultSTR = new string[10];
+                int counter = 0;
+
+                CardData cData = new CardData();
+
+                foreach (string val in splitSTR)
+                {
+                    if (!String.IsNullOrWhiteSpace(val))
+                    {
+                        resultSTR[counter] = val.Trim();
+
+                        Console.Write(resultSTR[counter] + ", ");
+                        counter++;
+                    }
+
+                   
+
+                    if(counter > 2)
+                    {
+                        Array.Resize(ref resultSTR, counter);
+
+                        cData.name = resultSTR[0];
+                        cData.ChannelName = resultSTR[1];
+                        cData.ChannelScheme = resultSTR[2];
+                        resultDict.Add(cData.name, cData);
+                    }
+
+
+                }
+                Console.WriteLine("");
+
+
+
+
+                string owner = "";
+                string ownerFormated = "";
+                Regex regEx = new Regex(@"\d");
+
+                //while(owner.Length < 4)
+               // {
+                    string name = cData.name;
+                    for ( int i = 0; i < cData.name.Length; i++)
+                    {
+                        if(regEx.IsMatch(name.Substring(i, 1)) & owner.Length < 4)
+                        {
+                            owner += name.Substring(i, 1);
+                        }
+
+                    }
+
+                if (!string.IsNullOrWhiteSpace(owner))
+                {
+                    ownerFormated = "/" + owner.Substring(0, 1) + "." + owner.Substring(1, 1) + "/" + owner.Substring(2, 1) + "." + owner.Substring(3, 1);
+                    cData.owner = ownerFormated;
+                }
+                else
+                {
+                    cData.owner = null;
+                }
+
+                //}
+                Console.WriteLine(ownerFormated);
+
+            }
+        }
+    }
+}
